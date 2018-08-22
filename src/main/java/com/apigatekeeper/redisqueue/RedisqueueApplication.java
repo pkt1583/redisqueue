f8fad07111af2295db834dd11aca5b269912e392
pankaj <tiwari.pankaj@gmail.com>
2018-08-22 07:22:09 +0530

    Initial commit

---
+++ src/main/java/com/apigatekeeper/redisqueue/RedisqueueApplication.java
@@ -0,0 +1,12 @@
+package com.apigatekeeper.redisqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisqueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisqueueApplication.class, args);
+    }
+}
